1df7c05dd7b32e6208117e920c7e910761a18721
Ray Haan <rayhaan@mvp-apps.ae>
2023-10-17 19:58:13 +0400

    update main api

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -4,13 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AchievementsController extends Controller
 {
-    public function index(User $user)
+    public function index(Request $request)
     {
+        $user = User::find($request->user);
+
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'User not found'], 404);
+        }
+        // The name of the tables
+        $achievementsTable = 'achievements';
+        $badgesTable = 'badges';
+
+        if (!Schema::hasTable($achievementsTable) || !Schema::hasTable($badgesTable)) {
+            // The table does not exist
+            return env('APP_ENV') === 'local' ?
+                response()->json(['success' => false, 'message' => 'Please run the migration command'], 404)
+                :
+                response()->json(['success' => false, 'message' => 'Something went wrong'], 404);
+        } else if (DB::table($achievementsTable)->count() === 0 || DB::table($badgesTable)->count() === 0) {
+            // The table is empty
+            return env('APP_ENV') === 'local' ?
+                response()->json(['success' => false, 'message' => 'Please seed the tables using {php artisan db:seed}'], 404)
+                :
+                response()->json(['success' => false, 'message' => 'Something went wrong'], 404);
+        }
+
         $unlockedAchievements = $user->achievements->pluck('name')->toArray();
-        $nextAvailableAchievements = $user->getNextAvailableAchievements(); // Implement this method in the User model.
+        $nextAvailableAchievements = $user->getNextAvailableAchievements();
         $currentBadge = $user->getCurrentBadge();
         $nextBadge = $user->getNextBadge();
         $remainingToUnlockNextBadge = $user->remainingAchievementsToUnlockNextBadge();
