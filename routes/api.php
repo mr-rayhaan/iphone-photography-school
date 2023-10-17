cf200628d3a490b366abda24a49f5dedfee8174a
Ray Haan <rayhaan@mvp-apps.ae>
2023-10-17 19:55:22 +0400

    create sample routes for testing

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\LessonUser;
+use App\Models\Lesson;
+use App\Models\User;
+use App\Models\Comment;
+
+use App\Events\LessonWatched;
+use App\Events\CommentWritten;
+
 use App\Http\Controllers\AchievementsController;
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +28,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/users/{user}/achievements', [AchievementsController::class, 'index']);
+
+Route::post('/watch-lesson', function(Request $request) {
+    
+    $lessonUser = LessonUser::create($request->only([
+        'user_id', 'lesson_id', 'watched'
+    ]));
+    $lesson = Lesson::find($lessonUser->lesson_id);
+    $user = User::find($lessonUser->user_id);
+
+    LessonWatched::dispatch($lesson, $user);
+});
+
+Route::post('/write-comment', function(Request $request) {
+
+    $comment = Comment::create($request->only([
+        'user_id', 'body'
+    ])); 
+    $user = User::find($comment->user_id);
+ 
+    CommentWritten::dispatch($comment, $user);
+});
