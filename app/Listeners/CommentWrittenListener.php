b6beb6a1bb6b7bdb6dc2222437bb0a982d1ee12f
Ray Haan <rayhaan@mvp-apps.ae>
2023-10-17 19:57:10 +0400

    create CommentWritten event and listener

--- app/Listeners/CommentWrittenListener.php
+++ app/Listeners/CommentWrittenListener.php
@@ -6,6 +6,7 @@ use App\Models\Achievement;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 
+
 class CommentWrittenListener
 {
     /**
@@ -21,15 +22,30 @@ class CommentWrittenListener
      */
     public function handle(object $event): void
     {
+
         $user = $event->user;
 
         // Logic to check the user's achievement progress and unlock relevant achievements.
-        // Example: Check if the user has written their first comment, then unlock the "First Comment Written" achievement.
         if ($user->commentsWritten() == 1) {
             $achievement = Achievement::where('name', 'First Comment Written')->first();
             $user->unlockAchievement($achievement);
         }
-        // Continue with similar logic for other achievements.
+        else if ($user->commentsWritten() == 5) {
+            $achievement = Achievement::where('name', '5 Comments Written')->first();
+            $user->unlockAchievement($achievement);
+        }
+        else if ($user->commentsWritten() == 10) {
+            $achievement = Achievement::where('name', '10 Comments Written')->first();
+            $user->unlockAchievement($achievement);
+        }
+        else if ($user->commentsWritten() == 25) {
+            $achievement = Achievement::where('name', '25 Comments Written')->first();
+            $user->unlockAchievement($achievement);
+        }
+        else if ($user->commentsWritten() == 50) {
+            $achievement = Achievement::where('name', '50 Comments Written')->first();
+            $user->unlockAchievement($achievement);
+        }
 
         // Check if the user has earned a new badge after unlocking achievements.
         $user->checkBadgeProgress();
